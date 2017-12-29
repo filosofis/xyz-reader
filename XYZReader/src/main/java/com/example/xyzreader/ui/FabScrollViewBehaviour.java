d09f8bcfdfb129aa29ff791729b7fb10691856a3
filosofis <olundqvist89@gmail.com>
2017-12-29 16:11:21 +0100

    Fab scroll behaviour

--- XYZReader/src/main/java/com/example/xyzreader/ui/FabScrollViewBehaviour.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/FabScrollViewBehaviour.java
@@ -2,74 +2,41 @@ package com.example.xyzreader.ui;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.View;
 
-import java.util.List;
 
-/**
- * As suggested by reviewer based on https://github.com/ianhanniballake/cheesesquare/tree/scroll_aware_fab/app/src/main/java/com/support/android/designlibdemo
- */
-
-public class FabScrollViewBehaviour extends AppBarLayout.ScrollingViewBehavior {
+public class FabScrollViewBehaviour extends CoordinatorLayout.Behavior<FloatingActionButton> {
     public FabScrollViewBehaviour(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
     @Override
-    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
-        return super.layoutDependsOn(parent, child, dependency) ||
-                dependency instanceof FloatingActionButton;
-    }
-
-
-    @Override
     public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout,
-                                       @NonNull View child,
+                                       @NonNull FloatingActionButton child,
                                        @NonNull View directTargetChild,
                                        @NonNull View target, int axes, int type) {
         return axes == ViewCompat.SCROLL_AXIS_VERTICAL ||
-                super.onStartNestedScroll(
-                coordinatorLayout,
-                child,
-                directTargetChild,
-                target, axes, type);
+                super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, axes, type);
     }
+
 
     @Override
     public void onNestedScroll(@NonNull CoordinatorLayout coordinatorLayout,
-                               @NonNull View child,
-                               @NonNull View target,
-                               int dxConsumed, int dyConsumed,
+                               @NonNull FloatingActionButton child,
+                               @NonNull View target, int dxConsumed, int dyConsumed,
                                int dxUnconsumed, int dyUnconsumed, int type) {
-        super.onNestedScroll(coordinatorLayout,
-                child,
-                target,
-                dxConsumed, dyConsumed,
-                dxUnconsumed, dyUnconsumed, type);
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type);
 
-        if (dyConsumed > 0) {
-            // User scrolled down -> hide the FAB
-            System.out.println("Attempting to hide FAB");
-            List<View> dependencies = coordinatorLayout.getDependencies(child);
-            for (View view : dependencies) {
-                if (view instanceof FloatingActionButton) {
-                    ((FloatingActionButton) view).hide();
-                }
-            }
-        } else if (dyConsumed < 0) {
-            // User scrolled up -> show the FAB
-            System.out.println("Attempting to show the FAB");
-            List<View> dependencies = coordinatorLayout.getDependencies(child);
-            for (View view : dependencies) {
-                if (view instanceof FloatingActionButton) {
-                    ((FloatingActionButton) view).show();
-                }
-            }
+        if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
+            System.out.println("Hiding");
+            child.hide();
+        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+            System.out.println("Showing");
+            child.show();
         }
     }
 }
